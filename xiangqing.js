b2bb383bed4594949b67b271d8b3f2a2b71ed597
POPOcard <123038500+POPOcard@users.noreply.github.com>
2023-05-05 14:34:34 +0800

    Update xiangqing.js

--- xiangqing.js
+++ xiangqing.js
@@ -1,4 +1,5 @@
-	const btn1 = document.getElementById('btn1');
+		
+const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');const btn4 = document.getElementById('btn4');
 const btn5 = document.getElementById('btn5');const btn6 = document.getElementById('btn6');
@@ -165,3 +166,4 @@ popups.forEach(function(popup) {
     }
   });
 });
+
