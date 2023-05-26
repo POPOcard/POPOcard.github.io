bd2acc0c4c50c24b4ffda2baabd83416090e7025
POPOcard <123038500+POPOcard@users.noreply.github.com>
2023-05-26 19:55:35 +0800

    更新 caidan2.js

--- caidan2.js
+++ caidan2.js
@@ -1,8 +1,8 @@
- // 在页面加载完成后执行 
+ 
  window.addEventListener('load', function() { 
-   // 添加 pagehide 事件监听器 
+    
    window.addEventListener('pagehide', function() { 
-     // 存储选项卡索引和滚动位置到 sessionStorage 
+     
      var activeTabIndex = getActiveTabIndex(); 
      var scrollPosition = window.pageYOffset; 
   
@@ -10,13 +10,13 @@
      sessionStorage.setItem('scrollPosition', scrollPosition); 
    }); 
   
-   // 添加 pageshow 事件监听器 
+   
    window.addEventListener('pageshow', function(event) { 
-     // 从 sessionStorage 中获取存储的选项卡索引和滚动位置 
+     
      var activeTabIndex = sessionStorage.getItem('activeTabIndex'); 
      var scrollPosition = sessionStorage.getItem('scrollPosition'); 
   
-     // 默认选项卡索引为 0，滚动位置为 0 
+     
      if (activeTabIndex === null) { 
        activeTabIndex = 0; 
      } 
@@ -24,22 +24,21 @@
        scrollPosition = 0; 
      } 
   
-     // 根据存储的选项卡索引，设置默认选中的选项卡 
+     
      var tabButtons = document.querySelectorAll('.tab-menu button'); 
      tabButtons[activeTabIndex].click(); 
   
-     // 将页面滚动到存储的位置 
      window.scrollTo(0, scrollPosition); 
    }); 
   
-   // 添加滚动事件监听器 
+   
    window.addEventListener('scroll', function() { 
-     // 存储滚动位置到 sessionStorage 
+     
      sessionStorage.setItem('scrollPosition', window.pageYOffset); 
    }); 
  }); 
   
- // 获取当前选中的选项卡索引 
+ 
  function getActiveTabIndex() { 
    var tabButtons = document.querySelectorAll('.tab-menu button'); 
    for (var i = 0; i < tabButtons.length; i++) { 
