029494d0a8fff4df0b875c44bb0b899b35847b71
POPOcard <123038500+POPOcard@users.noreply.github.com>
2023-05-28 06:50:44 +0800

    Create copy.js

---
+++ copy.js
@@ -0,0 +1,20 @@
+function copyText1() {
+      var textToCopy = "恩水通信";
+      copyTextToClipboard(textToCopy);
+    }
+
+    function copyText2() {
+      var textToCopy = "enshui1973";
+      copyTextToClipboard(textToCopy);
+    }
+
+    function copyTextToClipboard(text) {
+      var tempInput = document.createElement("input");
+      tempInput.setAttribute("value", text);
+      document.body.appendChild(tempInput);
+      tempInput.select();
+      document.execCommand("copy");
+      document.body.removeChild(tempInput);
+      alert("文字已复制成功！");
+        window.location.href = "weixin://";
+    }
