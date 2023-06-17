8ac6cc63b788a005abaed4f430a4d3b797223676
POPOcard <123038500+POPOcard@users.noreply.github.com>
2023-06-17 20:47:05 +0800

    更新 gonggao.js

--- js/gonggao.js
+++ js/gonggao.js
@@ -1,5 +1,5 @@
- // 检查本地存储中的时间戳 
-        var lastVisitTime = localStorage.getItem('lastVisitTime');
+    // 检查本地存储中的时间戳
+        var lastVisitTime = localStorage.getItem('announcementLastVisitTime');
 
         // 获取当前时间戳
         var currentTime = new Date().getTime();
@@ -7,14 +7,18 @@
         // 如果本地存储中没有时间戳或者当前时间距上次访问时间超过5分钟，则显示公告
         if (!lastVisitTime || currentTime - lastVisitTime > 5 * 60 * 1000) {
             // 显示公告
-            document.getElementById('announcement').style.display = 'block';
+            var announcementContainer = document.getElementById('announcement-container');
+            announcementContainer.style.display = 'block';
+        }
 
-            // 关闭公告
-            document.getElementById('closeBtn').addEventListener('click', function() {
-                // 隐藏公告
-                document.getElementById('announcement').style.display = 'none';
-                
+        // 关闭公告
+        var closeButtons = document.getElementsByClassName('close-button');
+        for (var i = 0; i < closeButtons.length; i++) {
+            closeButtons[i].addEventListener('click', function() {
+                var announcementContainer = document.getElementById('announcement-container');
+                announcementContainer.style.display = 'none';
+
                 // 更新本地存储的时间戳为当前时间
-                localStorage.setItem('lastVisitTime', currentTime);
+                localStorage.setItem('announcementLastVisitTime', currentTime);
             });
         }
