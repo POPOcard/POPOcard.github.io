2ef273b51dce1c8d4bdb436b56f8abf19f7ff363
POPOcard <123038500+POPOcard@users.noreply.github.com>
2023-06-17 14:54:59 +0800

    Update too.js

--- too.js
+++ too.js
@@ -12,30 +12,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
   popups.forEach(function(popup) {
     const closeBtn = document.createElement('button');
-closeBtn.classList.add('close-btn');
-closeBtn.textContent = '关闭此窗口';
-closeBtn.addEventListener('click', function() {
-  togglePopup(popup);
-});
-popup.appendChild(closeBtn);
+    closeBtn.classList.add('close-btn');
+    closeBtn.textContent = '关闭';
+    closeBtn.addEventListener('click', function() {
+      togglePopup(popup);
+    });
+    popup.appendChild(closeBtn);
 
-// 添加样式属性
-closeBtn.style.backgroundColor = "#0062cc;
-closeBtn.style.color = "transparent";
-closeBtn.style.webkitTextStroke = "0.05px black";
-closeBtn.style.webkitTextFillColor = "white";
-closeBtn.style.border = "none";
-closeBtn.style.padding = "10px 18px";
-closeBtn.style.borderRadius = "5px";
-closeBtn.style.marginTop = "5px";
-closeBtn.style.cursor = "pointer";
-closeBtn.style.fontSize = "23px";
-closeBtn.style.position = 'absolute';
+    // 添加样式属性
+    closeBtn.style.backgroundColor = "#a87f5f";
+    closeBtn.style.color = "transparent";
+    closeBtn.style.webkitTextStroke = "0.05px black";
+    closeBtn.style.webkitTextFillColor = "white";
+    closeBtn.style.border = "none";
+    closeBtn.style.padding = "10px 18px";
+    closeBtn.style.borderRadius = "5px";
+    closeBtn.style.marginTop = "5px";
+    closeBtn.style.cursor = "pointer";
+    closeBtn.style.fontSize = "23px";
+    closeBtn.style.position = 'absolute';
 closeBtn.style.right = '20px';
 closeBtn.style.bottom = '10px';
 closeBtn.style.display = 'block';
-
-
 
 
     popup.addEventListener('click', function(event) {
