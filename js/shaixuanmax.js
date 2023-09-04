df2b7076d11f0c663027bb5294a711b2f0f94d77
POPOcard <123038500+POPOcard@users.noreply.github.com>
2023-09-04 23:16:08 +0800

    更新 shaixuanmax.js

--- js/shaixuanmax.js
+++ js/shaixuanmax.js
@@ -1,43 +1,73 @@
-function filterAndDisplay(operator, packageType, minAges, deliveryAreas, rollover, broadband, location, number, niceNumber, selfActivate, videoMember, superData, superVoice, recommended, history) {
-    const allProducts = document.querySelectorAll('.product');
+const form = document.getElementById('filterFormContainer');
+const productsDiv = document.getElementById('products');
 
-    for (const product of allProducts) {
-        const productOperator = product.getAttribute('data-operator');
-        const productPackageType = product.getAttribute('data-package-type');
-        const productMinAges = product.getAttribute('data-min-age').split(' '); // 分割成年龄数组
-        const productDeliveryAreas = product.getAttribute('data-delivery-areas').split(' '); // 分割成地区数组
-        const productRollover = product.getAttribute('data-rollover') === 'true';
-        const productBroadband = product.getAttribute('data-broadband') === 'true';
-        const productLocation = product.getAttribute('data-location') === 'true';
-        const productNumber = product.getAttribute('data-number') === 'true';
-        const productNiceNumber = product.getAttribute('data-nice-number') === 'true';
-        const productSelfActivate = product.getAttribute('data-self-activate') === 'true';
-        const productVideoMember = product.getAttribute('data-video-member') === 'true';
-        const productSuperData = product.getAttribute('data-super-data') === 'true';
-        const productSuperVoice = product.getAttribute('data-super-voice') === 'true';
-        const productRecommended = product.getAttribute('data-recommended') === 'true';
-        const productHistory = product.getAttribute('data-history') === 'true';
+form.addEventListener('change', function (event) {
+  event.preventDefault();
 
-        if (
-            (operator === 'all' || productOperator === operator) &&
-            (packageType === 'all' || productPackageType === packageType) &&
-            (minAges.includes('all') || minAges.every(age => productMinAges.includes(age))) && // 所有选择的年龄都必须包含在商品的年龄中
-            (deliveryAreas.includes('all') || deliveryAreas.every(area => productDeliveryAreas.includes(area))) && // 所有选择的地区都必须包含在商品的地区中
-            (!rollover || productRollover) &&
-            (!broadband || productBroadband) &&
-            (!location || productLocation) &&
-            (!number || productNumber) &&
-            (!niceNumber || productNiceNumber) &&
-            (!selfActivate || productSelfActivate) &&
-            (!videoMember || productVideoMember) &&
-            (!superData || productSuperData) &&
-            (!superVoice || productSuperVoice) &&
-            (!recommended || productRecommended) &&
-            (!history || productHistory)
-        ) {
-            product.style.display = 'block'; // 显示符合条件的商品
-        } else {
-            product.style.display = 'none'; // 隐藏不符合条件的商品
-        }
+  const operator = getSelectedValues('operator');
+  const packageType = getSelectedValues('packageType');
+  const minAge = getSelectedValues('minAge');
+  const deliveryArea = getSelectedValues('deliveryArea');
+  const rollover = document.querySelector('input[name="rollover"]').checked;
+  const broadband = document.querySelector('input[name="broadband"]').checked;
+  const location = document.querySelector('input[name="location"]').checked;
+  const number = document.querySelector('input[name="number"]').checked;
+  const niceNumber = document.querySelector('input[name="niceNumber"]').checked;
+  const selfActivate = document.querySelector('input[name="selfActivate"]').checked;
+  const videoMember = document.querySelector('input[name="videoMember"]').checked;
+  const superData = document.querySelector('input[name="superData"]').checked;
+  const superVoice = document.querySelector('input[name="superVoice"]').checked;
+  const recommended = document.querySelector('input[name="recommended"]').checked;
+  const history = document.querySelector('input[name="history"]').checked;
+
+  filterAndDisplay(operator, packageType, minAge, deliveryArea, rollover, broadband, location, number, niceNumber, selfActivate, videoMember, superData, superVoice, recommended, history);
+});
+
+function getSelectedValues(name) {
+  const selectedCheckboxes = Array.from(document.querySelectorAll(`input[name="${name}"]:checked`));
+  return selectedCheckboxes.map(checkbox => checkbox.value);
+}
+
+function filterAndDisplay(operator, packageType, minAge, deliveryArea, rollover, broadband, location, number, niceNumber, selfActivate, videoMember, superData, superVoice, recommended, history) {
+  const allProducts = document.querySelectorAll('.product');
+
+  allProducts.forEach(product => {
+    const productOperator = product.getAttribute('data-operator');
+    const productPackageType = product.getAttribute('data-package-type');
+    const productMinAge = product.getAttribute('data-min-age').split(' ');
+    const productDeliveryArea = product.getAttribute('data-delivery-area').split(' ');
+    const productRollover = product.getAttribute('data-rollover') === 'true';
+    const productBroadband = product.getAttribute('data-broadband') === 'true';
+    const productLocation = product.getAttribute('data-location') === 'true';
+    const productNumber = product.getAttribute('data-number') === 'true';
+    const productNiceNumber = product.getAttribute('data-nice-number') === 'true';
+    const productSelfActivate = product.getAttribute('data-self-activate') === 'true';
+    const productVideoMember = product.getAttribute('data-video-member') === 'true';
+    const productSuperData = product.getAttribute('data-super-data') === 'true';
+    const productSuperVoice = product.getAttribute('data-super-voice') === 'true';
+    const productRecommended = product.getAttribute('data-recommended') === 'true';
+    const productHistory = product.getAttribute('data-history') === 'true';
+
+    if (
+      (operator.includes('all') || operator.includes(productOperator)) &&
+      (packageType.includes('all') || packageType.includes(productPackageType)) &&
+      (minAge.includes('all') || minAge.some(age => productMinAge.includes(age))) &&
+      (deliveryArea.includes('all') || deliveryArea.some(area => productDeliveryArea.includes(area))) &&
+      (!rollover || productRollover) &&
+      (!broadband || productBroadband) &&
+      (!location || productLocation) &&
+      (!number || productNumber) &&
+      (!niceNumber || productNiceNumber) &&
+      (!selfActivate || productSelfActivate) &&
+      (!videoMember || productVideoMember) &&
+      (!superData || productSuperData) &&
+      (!superVoice || productSuperVoice) &&
+      (!recommended || productRecommended) &&
+      (!history || productHistory)
+    ) {
+      product.style.display = 'block';
+    } else {
+      product.style.display = 'none';
     }
+  });
 }
