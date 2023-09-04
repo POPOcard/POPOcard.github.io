2c4b7b3c2468c06b24dac81cf67ac05831d76f0c
POPOcard <123038500+POPOcard@users.noreply.github.com>
2023-09-04 23:06:51 +0800

    更新 shaixuanmax.js

--- js/shaixuanmax.js
+++ js/shaixuanmax.js
@@ -1,73 +1,43 @@
-const form = document.getElementById('filterFormContainer');
-const productsDiv = document.getElementById('products');
-
-form.addEventListener('change', function(event) {
-  event.preventDefault();
-
-  const operator = document.querySelector('input[name="operator"]:checked').value;
-  const packageType = document.querySelector('input[name="packageType"]:checked').value;
-  const minAges = getSelectedCheckboxes('minAge');
-  const deliveryAreas = getSelectedCheckboxes('deliveryArea');
-  const rollover = document.querySelector('input[name="rollover"]').checked;
-  const broadband = document.querySelector('input[name="broadband"]').checked;
-  const location = document.querySelector('input[name="location"]').checked;
-  const number = document.querySelector('input[name="number"]').checked;
-  const niceNumber = document.querySelector('input[name="niceNumber"]').checked;
-  const selfActivate = document.querySelector('input[name="selfActivate"]').checked;
-  const videoMember = document.querySelector('input[name="videoMember"]').checked;
-  const superData = document.querySelector('input[name="superData"]').checked;
-  const superVoice = document.querySelector('input[name="superVoice"]').checked;
-  const recommended = document.querySelector('input[name="recommended"]').checked;
-  const history = document.querySelector('input[name="history"]').checked;
-
-  filterAndDisplay(operator, packageType, minAges, deliveryAreas, rollover, broadband, location, number, niceNumber, selfActivate, videoMember, superData, superVoice, recommended, history);
-});
-
-function getSelectedCheckboxes(name) {
-  const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
-  return Array.from(checkboxes).map(checkbox => checkbox.value);
-}
-
 function filterAndDisplay(operator, packageType, minAges, deliveryAreas, rollover, broadband, location, number, niceNumber, selfActivate, videoMember, superData, superVoice, recommended, history) {
-  const allProducts = document.querySelectorAll('.product');
+    const allProducts = document.querySelectorAll('.product');
 
-  for (const product of allProducts) {
-    const productOperator = product.getAttribute('data-operator');
-    const productPackageType = product.getAttribute('data-package-type');
-    const productMinAges = product.getAttribute('data-min-age').split(' ');
-    const productDeliveryAreas = product.getAttribute('data-delivery-areas').split(' ');
-    const productRollover = product.getAttribute('data-rollover') === 'true';
-    const productBroadband = product.getAttribute('data-broadband') === 'true';
-    const productLocation = product.getAttribute('data-location') === 'true';
-    const productNumber = product.getAttribute('data-number') === 'true';
-    const productNiceNumber = product.getAttribute('data-nice-number') === 'true';
-    const productSelfActivate = product.getAttribute('data-self-activate') === 'true';
-    const productVideoMember = product.getAttribute('data-video-member') === 'true';
-    const productSuperData = product.getAttribute('data-super-data') === 'true';
-    const productSuperVoice = product.getAttribute('data-super-voice') === 'true';
-    const productRecommended = product.getAttribute('data-recommended') === 'true';
-    const productHistory = product.getAttribute('data-history') === 'true';
+    for (const product of allProducts) {
+        const productOperator = product.getAttribute('data-operator');
+        const productPackageType = product.getAttribute('data-package-type');
+        const productMinAges = product.getAttribute('data-min-age').split(' '); // 分割成年龄数组
+        const productDeliveryAreas = product.getAttribute('data-delivery-areas').split(' '); // 分割成地区数组
+        const productRollover = product.getAttribute('data-rollover') === 'true';
+        const productBroadband = product.getAttribute('data-broadband') === 'true';
+        const productLocation = product.getAttribute('data-location') === 'true';
+        const productNumber = product.getAttribute('data-number') === 'true';
+        const productNiceNumber = product.getAttribute('data-nice-number') === 'true';
+        const productSelfActivate = product.getAttribute('data-self-activate') === 'true';
+        const productVideoMember = product.getAttribute('data-video-member') === 'true';
+        const productSuperData = product.getAttribute('data-super-data') === 'true';
+        const productSuperVoice = product.getAttribute('data-super-voice') === 'true';
+        const productRecommended = product.getAttribute('data-recommended') === 'true';
+        const productHistory = product.getAttribute('data-history') === 'true';
 
-    if (
-      (operator === 'all' || productOperator === operator) &&
-      (packageType === 'all' || productPackageType === packageType) &&
-      (minAges.includes('all') || minAges.some(age => productMinAges.includes(age))) &&
-      (deliveryAreas.includes('all') || deliveryAreas.some(area => productDeliveryAreas.includes(area))) &&
-      (!rollover || productRollover) &&
-      (!broadband || productBroadband) &&
-      (!location || productLocation) &&
-      (!number || productNumber) &&
-      (!niceNumber || productNiceNumber) &&
-      (!selfActivate || productSelfActivate) &&
-      (!videoMember || productVideoMember) &&
-      (!superData || productSuperData) &&
-      (!superVoice || productSuperVoice) &&
-      (!recommended || productRecommended) &&
-      (!history || productHistory)
-    ) {
-      product.style.display = 'block';
-    } else {
-      product.style.display = 'none';
+        if (
+            (operator === 'all' || productOperator === operator) &&
+            (packageType === 'all' || productPackageType === packageType) &&
+            (minAges.includes('all') || minAges.every(age => productMinAges.includes(age))) && // 所有选择的年龄都必须包含在商品的年龄中
+            (deliveryAreas.includes('all') || deliveryAreas.every(area => productDeliveryAreas.includes(area))) && // 所有选择的地区都必须包含在商品的地区中
+            (!rollover || productRollover) &&
+            (!broadband || productBroadband) &&
+            (!location || productLocation) &&
+            (!number || productNumber) &&
+            (!niceNumber || productNiceNumber) &&
+            (!selfActivate || productSelfActivate) &&
+            (!videoMember || productVideoMember) &&
+            (!superData || productSuperData) &&
+            (!superVoice || productSuperVoice) &&
+            (!recommended || productRecommended) &&
+            (!history || productHistory)
+        ) {
+            product.style.display = 'block'; // 显示符合条件的商品
+        } else {
+            product.style.display = 'none'; // 隐藏不符合条件的商品
+        }
     }
-  }
 }
